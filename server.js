5536f48f6c8103c4c3cc103eb8e15e321845f187
checkr <ugochiori@gmail.com>
2025-10-08 17:20:10 +0100

    corr

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   console.log("Incoming:", req.method, req.url);
   next();
 });
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
